66816b8e0445aa46845bc3836d80a59fdaf233c9
Sarvagya Verma <sarvagya1115@gmail.com>
2022-01-21 07:23:39 +0530

    Added Open Weather Map API Call

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apiKey = 'e2490eb92936c771bfa507d54ebaa14b';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,45 +11,47 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   void getLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.getLatitude();
+    longitude = location.getLongitude();
+    getData();
+  }
 
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
+  void getData() async {
+    Uri url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      String cityName = decodedData['name'];
+      int condition = decodedData['weather'][0]['id'];
+
+      print(temperature);
+      print(cityName);
+      print(condition);
+    } else {
+      print(response.statusCode);
     }
+  }
 
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-
-    print(position);
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
